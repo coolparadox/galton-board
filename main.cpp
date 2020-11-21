e03035fac08c9c4ad8edbfe99b2bc6e5068a0787
Rafael Lorandi <coolparadox@gmain.com>
2020-11-21 12:17:30 -0300

    Empty GTK window

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
-#include <iostream>
+#include <gtkmm.h>
 
-int main() {
-    std::cout << "Hello world" << std::endl;
+int main(int argc, char *argv[])
+{
+    Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "org.gtkmm.examples.base");
+    Gtk::Window window;
+    window.set_default_size(200, 200);
+    return app->run(window);
 }
